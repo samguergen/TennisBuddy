5229d7091dc7e0b5f4ec78b1ea04229d4c742abf
Samantha Guergenenov <samguergen@gmail.com>
2016-04-19 22:28:55 -0400

    correct sass import

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module KillAMockingJay
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets", "stylesheets")
+    config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets","fonts")
+
   end
 end
