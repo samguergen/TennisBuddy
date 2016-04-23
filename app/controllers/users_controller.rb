8d6de0f3b2e4facfe1e3459c5fb1c8b23baca493
Samantha Guergenenov <samguergen@gmail.com>
2016-04-23 17:27:15 -0400

    post action on signup to create method in users controller but auth prob

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :first_name, :last_name, :age)
+  end
+
+end
