11abdbb3fe3a64cdf9b770bbba909807750d6e6f
Samantha Guergenenov <samguergen@gmail.com>
2016-04-23 15:55:26 -0400

    resources for models

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root 'application#index'
   get '*path' => 'application#index'
+
+  resources :users, :games, :comments
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
