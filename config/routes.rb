0a819d562f7dacae8d3c5ea66f3b4b6ba9d99e52
Samantha Guergenenov <samguergen@gmail.com>
2016-04-26 11:59:16 -0400

    login logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'application#index'
   get '*path' => 'application#index'
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users, :games, :comments
 
   # You can have the root of your site routed with "root"
