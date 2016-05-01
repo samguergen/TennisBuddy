1db2530539d9ec2c4386befc4174720f78700350
Samantha Guergenenov <samguergen@gmail.com>
2016-05-01 14:02:24 -0400

    login form showing but user model doesnt like attr acess

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def login
+    current_user = User.find_by(email => params[:email], password => params[:password])
+    if current_user
+      session[user_id] = current_user.id
+      puts 'LOGGED IN!'
+      redirect_to '/'
+    else
+      flash[:alert] = "There was a problem with your username and password."
+      puts "Problem"
+      redirect_to '/login'
+    end
+
+  end
+
+  def logout
+  end
+
+
 end
