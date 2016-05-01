fab6c25e3763539d35ca8db88d32436ba6451f63
Samantha Guergenenov <samguergen@gmail.com>
2016-05-01 14:13:45 -0400

    perfectly able to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,33 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # def login
+  #   current_user = User.find_by(email => params[:email], password => params[:password])
+  #   if current_user
+  #     session[user_id] = current_user.id
+  #     puts 'LOGGED IN!'
+  #     redirect_to '/'
+  #   else
+  #     flash[:alert] = "There was a problem with your username and password."
+  #     puts "Problem"
+  #     redirect_to '/login'
+  #   end
+
+  # end
+
+
   def login
-    current_user = User.find_by(email => params[:email], password => params[:password])
-    if current_user
-      session[user_id] = current_user.id
-      puts 'LOGGED IN!'
+        user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
       redirect_to '/'
     else
-      flash[:alert] = "There was a problem with your username and password."
-      puts "Problem"
+    # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
     end
-
   end
 
   def logout
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
   has_secure_password
-  validates_presence_of :password, :on => :create
+  # attr_accessible :email, :password, :password_confirmation
+  # has_secure_password
+  # validates_presence_of :password, :on => :create
   has_many :games
   has_many :comments
 end
