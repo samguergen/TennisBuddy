47bb15491b2d038da8ad16c6abd6b76176a1ef3c
Samantha Guergenenov <samguergen@gmail.com>
2016-05-01 13:52:31 -0400

    re added some custom session routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,4 @@
 class SessionsController < ApplicationController
   def new
   end
-
-  def create
-    @session = User.find_by(:email => user[email], :password => user[password])
-    if @session
-      flash[:notice] = "You are now logged in"
-    else
-      flash[:warning] = "You did not use the correct email/password combination"
-  end
-
 end
