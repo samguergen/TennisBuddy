3eda5d919a18ee4ef24b2a345e297ef850122417
Samantha Guergenenov <samguergen@gmail.com>
2016-05-04 10:41:14 -0400

    redirect to new game partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
 
   def new
+    redirect_to '/new-game'
   end
 
   def logged
     @u = User.find(session[:user_id])
-    puts 'user in logged '
+    puts 'inside logged func'
     puts u
     redirect_to '/browse'
   end
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      puts 'inside login func'
+      redirect_to '/new-game'
     else
       redirect_to '/login'
     end
