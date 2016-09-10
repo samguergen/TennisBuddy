dfa742fe84846235ca8f20f223bf7d5e824bf709
Samantha Guergenenov <samguergen@gmail.com>
2016-09-10 19:29:53 -0400

    comment srverjs merge

--- tennis-buddy/app.js
+++ tennis-buddy/app.js
@@ -64,5 +64,41 @@ app.use(function(err, req, res, next) {
 });
 
 
+// // configuration ===========================================
+
+// // config files
+// var db = require('./config/db');
+
+// // set our port
+// var port = process.env.PORT || 8080;
+
+// // connect to our mongoDB database
+// // (uncomment after you enter in your own credentials in config/db.js)
+// // mongoose.connect(db.url);
+
+// // get all data/stuff of the body (POST) parameters
+// // parse application/json
+// app.use(bodyParser.json());
+
+// // parse application/vnd.api+json as json
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// // routes ==================================================
+// require('./app/routes')(app); // configure our routes
+
+// // start app ===============================================
+// // startup our app at http://localhost:8080
+// app.listen(port);
+
+// // shoutout to the user
+// console.log('Magic happens on port ' + port);
+
+
+
 module.exports = app;
 
