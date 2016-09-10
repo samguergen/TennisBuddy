68b7c75efc2cb73cb8368c0ed8a27fbdc933bb24
Samantha Guergenenov <samguergen@gmail.com>
2016-09-10 16:13:56 -0400

    add rest routes

--- tennis-buddy/routes/index.js
+++ tennis-buddy/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Game = mongoose.model('Game');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/games', function(req, res, next) {
+  Game.find(function(err, games){
+    if(err){ return next(err); }
+
+    res.json(games);
+  });
+});
+
 
 module.exports = router;
