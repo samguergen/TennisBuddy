22131d6bb1b70342d7af6d5731e6c03668ca65fd
Samantha Guergenenov <samguergen@gmail.com>
2016-09-14 19:14:22 -0400

    popup overlays

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,5 +2,9 @@ angular
     .module('myApp', [])
     .controller('MainCtrl', ['$scope', '$location', function($scope, $location){
       $scope.intro = "Angular is working";
-      alert('inside angular ctrl');
+     // alert('inside angular ctrl');
+
+      $scope.openLoginBox = function(){
+
+      }
     }]);
