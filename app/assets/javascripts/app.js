03a8d039c4cffb43cb50a9eb747c9ebfd6db36d1
Samantha Guergenenov <samguergen@gmail.com>
2016-09-14 18:48:40 -0400

    angular loaded

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,6 @@
+angular
+    .module('myApp', [])
+    .controller('MainCtrl', ['$scope', '$location', function($scope, $location){
+      $scope.intro = "Angular is working";
+      alert('inside angular ctrl');
+    }]);
---
+++ app/assets/javascripts/ng/ngCtrl.js
@@ -0,0 +1,5 @@
+// angular.module('myApp')
+// .controller('HomeCtrl', ['$scope', '$location', function($scope, $location){
+//   $scope.intro = "Angular is working";
+//   console.log('inside angular ctrl');
+// }])
