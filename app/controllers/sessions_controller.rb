636897f45675ce2ca6b244e17db3216cc0dacdfd
Samantha Guergenenov <samguergen@gmail.com>
2016-09-15 14:44:04 -0400

    bcrypted installed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+  protect_from_forgery
 
   def login
     @user = User.find_by({email: params[:email]})
     if @user.try(:authenticate, params[:password])
       session[:user_id] = @user.id
-      redirect_to "/"
+      alert("you logged in succesfully")
+      redirect_to welcome_index_path(@user)
     else
+      flash[:alert] = "You did not log in"
+      puts "Not logged in"
       redirect_to '/'
     end
   end
