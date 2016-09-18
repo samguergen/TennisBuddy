e2dcbd4b4296cbf4316c981f07a38df35246f343
Samantha Guergenenov <samguergen@gmail.com>
2016-09-18 02:14:30 -0400

    could retrieve params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   protect_from_forgery
 
   def login
-    @user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if @user && @user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around our website.
+    @user = User.find_by_email(params[:user][:email])
+    puts 'user is '
+    puts @user
+    if @user && @user.authenticate(params[:user][:password])
+      puts 'logged in'
       session[:user_id] = @user.id
       puts "successfully logged in"
       redirect_to '/'
