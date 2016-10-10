6ac435687f00087bb77b8bb7055978c2de2f47e8
Samantha Guergenenov <samguergen@gmail.com>
2016-10-10 17:00:44 -0400

    ok pozitioning for user profile page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,42 +6,8 @@ angular
       $scope.showLogin = false;
       $scope.showSignup = false;
       $scope.showProfile = false;
-      $scope.desiredLoc = "";
+      $scope.lookingToPlay = false;
       $scope.openGameForm = true;
-
-      $scope.startModal = function(order){
-        var modal, btn, span;
-
-        if (order == 1){
-          console.log('inside order 1', order);
-          modal = document.getElementById('myModal');
-          btn = document.getElementById("myBtn");
-          span = document.getElementsByClassName("close")[0];
-        }
-        else if(order == 2){
-          console.log('inside order 2', order);
-          modal = document.getElementById('mySecondModal');
-          btn = document.getElementByClassName("submit-location")[0];
-          span = document.getElementsByClassName("secondClose")[0];
-        }
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-
-      }
-
-      $scope.closeModal = function() {
-            var modal = document.getElementById('myModal');
-            modal.style.display = "none";
-        }
 
       $scope.reloadRoute = function(endpoint) {
         console.log("inside reloadroute");
@@ -49,7 +15,7 @@ angular
          $window.location.href = '/'+ endpoint;
       }
 
-
-      $scope.navLinksOpen = false;
+      $scope.sendMessage = function(){
+      }
 
     }]);
