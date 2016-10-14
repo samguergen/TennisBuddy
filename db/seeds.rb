f7bc901791c65e59ee0d81bcdac9986718b38514
Samantha Guergenenov <samguergen@gmail.com>
2016-10-14 16:03:28 -0400

    finished seed for first 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+first_user = User.create({
+  first_name: "Elsa",
+  last_name: "Banana",
+  age: 25,
+  gender: "female",
+  description: "A very cool girl who loves tennis",
+  email: "banana@gmail.com",
+  password: "123123",
+  password_confirmation: "123123"
+})
+
+first_game = Game.create({
+  title: "Morning Game in Manhattan",
+  description: "Anyone is up for an early morning game in Central Park?",
+  score: "no scores yet",
+  creator: first_user
+})
+
+first_comment = Comment.create({
+  commenter: "Julie",
+  body: "This looks like a great game",
+  game: first_game
+  })
+
+
+second_user = User.create({
+  first_name: "Leila",
+  last_name: "Malawi",
+  age: 29,
+  gender: "female",
+  description: "Awesome human. I am just a great person. Hit me up if you ever want to play",
+  email: "leila@gmail.com",
+  password: "234234",
+  password_confirmation: "234234"
+})
+
+second_game = Game.create({
+  title: "Afternoon game anyone?",
+  description: "Looking to play for at least 2 hours. Females only pls.",
+  score: "no scores yet",
+  creator: second_user
+})
+
+second_comment = Comment.create({
+  commenter: "Emma",
+  body: "I wish I had arms so I could play with you..",
+  game: second_game
+  })
+
+
+third_user = User.create({
+  first_name: "Yulia",
+  last_name: "Eskanov",
+  age: 19,
+  gender: "female",
+  description: "Me looking for player of tennis, please email me for play",
+  email: "yulia@vkontakt.ru",
+  password: "234234",
+  password_confirmation: "234234"
+})
+
+third_game = Game.create({
+  title: "Me would like play",
+  description: "I want to play in rich suburb of NY. Please be fre to play tennis with me in rich suburb.",
+  score: "no scores yet",
+  creator: third_user
+})
+
+third_comment = Comment.create({
+  commenter: "Olga",
+  body: "I want to play with you, comrade.",
+  game: third_game
+  })
