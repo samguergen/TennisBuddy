6a2aa48ddbc87f5582e775f10fdaf7f839a364d8
Samantha Guergenenov <samguergen@gmail.com>
2016-10-23 16:26:15 -0400

    all good seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,21 +61,13 @@ first_user = {
 
 first_create = User.create(first_user)
 
-# first_create.games.create(title: "Morning Game in Manhattan",
-#   description: "Anyone is up for an early morning game in Central Park?",
-#   score: "no scores yet");
-
 first_game = {
   title: "Morning Game in Manhattan",
   description: "Anyone is up for an early morning game in Central Park?",
   score: "no scores yet",
-  # user_id: first_create
 }
 
-
-first_create.games.create(first_game);
-
-# Game.create(first_game)
+first_create.games.create(first_game)
 
 first_comment = {
   commenter: "Julie",
@@ -86,62 +78,60 @@ first_comment = {
 Comment.create(first_comment)
 
 
-# second_user = {
-#   first_name: "Leila",
-#   last_name: "Malawi",
-#   age: 29,
-#   gender: "female",
-#   description: "Awesome human. I am just a great person. Hit me up if you ever want to play",
-#   email: "leila@gmail.com",
-#   password: "234234",
-#   password_confirmation: "234234"
-# }
+second_user = {
+  first_name: "Leila",
+  last_name: "Malawi",
+  age: 29,
+  gender: "female",
+  description: "Awesome human. I am just a great person. Hit me up if you ever want to play",
+  email: "leila@gmail.com",
+  password: "234234",
+  password_confirmation: "234234"
+}
 
-# User.create(second_user)
+second_create = User.create(second_user)
 
-# second_game = {
-#   title: "Afternoon game anyone?",
-#   description: "Looking to play for at least 2 hours. Females only pls.",
-#   score: "no scores yet",
-#   user_id: second_user
-# }
+second_game = {
+  title: "Afternoon game anyone?",
+  description: "Looking to play for at least 2 hours. Females only pls.",
+  score: "no scores yet"
+}
 
-# Game.create(second_game)
+second_create.games.create(second_game)
 
-# second_comment = {
-#   commenter: "Emma",
-#   body: "I wish I had arms so I could play with you..",
-#   game_id: second_game
-#   }
+second_comment = {
+  commenter: "Emma",
+  body: "I wish I had arms so I could play with you..",
+  game_id: second_game
+  }
 
-# Comment.create(second_comment)
+Comment.create(second_comment)
 
-# third_user = {
-#   first_name: "Yulia",
-#   last_name: "Eskanov",
-#   age: 19,
-#   gender: "female",
-#   description: "Me looking for player of tennis, please email me for play",
-#   email: "yulia@vkontakt.ru",
-#   password: "234234",
-#   password_confirmation: "234234"
-# }
+third_user = {
+  first_name: "Yulia",
+  last_name: "Eskanov",
+  age: 19,
+  gender: "female",
+  description: "Me looking for player of tennis, please email me for play",
+  email: "yulia@vkontakt.ru",
+  password: "234234",
+  password_confirmation: "234234"
+}
 
-# User.create(third_user)
+third_create = User.create(third_user)
 
-# third_game = {
-#   title: "Me would like play",
-#   description: "I want to play in rich suburb of NY. Please be fre to play tennis with me in rich suburb.",
-#   score: "no scores yet",
-#   user_id: third_user
-# }
+third_game = {
+  title: "Me would like play",
+  description: "I want to play in rich suburb of NY. Please be fre to play tennis with me in rich suburb.",
+  score: "no scores yet"
+}
 
-# Game.create(third_game)
+third_create.games.create(third_game)
 
-# third_comment = {
-#   commenter: "Olga",
-#   body: "I want to play with you, comrade.",
-#   game_id: third_game
-#   }
+third_comment = {
+  commenter: "Olga",
+  body: "I want to play with you, comrade.",
+  game_id: third_game
+  }
 
-# Comment.create(third_comment)
+Comment.create(third_comment)
