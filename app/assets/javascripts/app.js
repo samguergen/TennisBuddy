4950f55c078e16526503e0c801a66566564d0914
Samantha Guergenenov <samguergen@gmail.com>
2016-11-16 19:03:25 -0500

    consolelogging popup vars

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,22 +3,19 @@ angular
     .controller('MainCtrl', ['$scope', '$location', '$window', function($scope, $location, $window){
 
       console.log('init main ng controller');
+
       $scope.showLogin = false;
       $scope.showSignup = false;
       $scope.showProfile = false;
       $scope.lookingToPlay = false;
       $scope.openGameForm = true;
 
+      console.log('vars are ', $scope.showLogin, $scope.showProfile, $scope.showProfile);
+
       $scope.reloadRoute = function(endpoint) {
         console.log("inside reloadroute");
          $window.location.reload();
          $window.location.href = endpoint;
       }
-
-      // $scope.init = function(){
-      //   console.log('reload once');
-      //   $window.location.reload();
-      // }
-      // $scope.init();
 
     }]);
