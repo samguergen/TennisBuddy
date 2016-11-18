f7bfdb95106e3ab4e3d4a24dd6e445efbfc562df
Samantha Guergenenov <samguergen@gmail.com>
2016-11-18 14:05:03 -0500

    gmap for user show displaying on click

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,4 +19,22 @@ angular
          $window.location.href = endpoint;
       }
 
+      $scope.initialize = function(coor) {
+        var coords = [40.78694, -73.97528];
+        console.log('inside initMap in show page');
+        console.log(coords);
+        console.log('real coords are ', coor);
+        var uluru = {lat: coords[0], lng: coords[1]};
+        // var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('showmap'), {
+          // zoom: 4,
+          zoom: 14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
     }]);
