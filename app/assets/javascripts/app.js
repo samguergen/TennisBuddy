40d1d661148b9e5215d37284a13fbf5187b64874
Samantha Guergenenov <samguergen@gmail.com>
2016-11-19 16:00:06 -0500

    passing info from form somewhat

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,7 @@
 angular
     .module('myApp', [])
     .controller('MainCtrl', ['$scope', '$location', '$window', '$http', function($scope, $location, $window, $http){
+       $http.defaults.headers.common["X-Requested-With"];
 
       console.log('angular installed? ', angular);
 
@@ -9,10 +10,10 @@ angular
       $scope.showProfile = false;
       $scope.lookingToPlay = false;
       $scope.openGameForm = true;
-      // $scope.lat = 0;
-      // $scope.lng = 0;
+      // $scope.game = {};
+      $scope.game = {title: "", description: "", player_1: "", address: "", townCountry: "", coordinates: ""};
 
-      console.log('vars are ', $scope.showLogin, $scope.showProfile, $scope.showProfile);
+      // console.log('vars are ', $scope.showLogin, $scope.showProfile, $scope.showProfile);
 
       $scope.reloadRoute = function(endpoint) {
          $window.location.reload();
@@ -32,39 +33,24 @@ angular
         });
       }
 
-      $scope.get_coords = function(address) {
-        console.log('inside get_coords func');
-        console.log('address is ', address);
-        var gc      = new google.maps.Geocoder(),
-            opts    = { 'address' : address };
 
-        gc.geocode(opts, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {   
-                var loc     = results[0].geometry.location,
-                    lat     = loc.$a,
-                    long    = loc.ab;
-
-                // Success.  Do stuff here.
-                console.log('lat/longs are ', loc, lat);
-                console.log('success');
-            }
-            else {
-                // Ruh roh.  Output error stuff here
-                console.log('error');
-            }
-        });
+    $scope.createGame = function(userInput) {
+      console.log('userInput is ', userInput);
+      alert('userInput is ', userInput);
+      console.log('game from ngModel is ', $scope.game);
+      console.log('description is ', $scope.game.description);
     }
 
-    $scope.getCoords = function(address, townCountry){
-      var slugAddress = address.replace(/ |,/g , '+');
-      var slugTownCountry = townCountry.replace(/ |,/g , '+');
-      var allSlug = slugAddress + ',' + slugTownCountry;
-      var apiEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + allSlug + '&key=AIzaSyC5TZNWeG6UJI4Gw8fTsaaHkd3rV7Qy5p8'
-      console.log('api endpoint is ', apiEndpoint);
+    // $scope.getCoords = function(address, townCountry){
+    //   var slugAddress = address.replace(/ |,/g , '+');
+    //   var slugTownCountry = townCountry.replace(/ |,/g , '+');
+    //   var allSlug = slugAddress + ',' + slugTownCountry;
+    //   var apiEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + allSlug + '&key=AIzaSyC5TZNWeG6UJI4Gw8fTsaaHkd3rV7Qy5p8'
+    //   console.log('api endpoint is ', apiEndpoint);
 
-      // $http.post(apiEndpoint)
+    //   // $http.post(apiEndpoint)
 
-    }
+    // }
 
     $scope.getLatLng = function(address, townCountry) {
       var slugAddress = address.replace(/ |,/g , '+');
@@ -81,7 +67,20 @@ angular
         $scope.lng = longitude;
         console.log('latitude is ', latitude, 'lng is ', longitude);
         } 
-      }); 
+      });
+      $http({
+            method: 'POST',
+            url: 'http://localhost:3000/game',
+            data: game,
+            // data: {'game': {'name': 'John Smith', 'message': 'Hello'}},
+            headers: {'Content-Type': 'application/json'}
+        }).then(function mySucces(response) {
+        $scope.myResponse = response.data;
+        console.log('response is ', response.data);
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+
     }
 
     }]);
