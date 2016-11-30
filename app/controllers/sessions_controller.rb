982c9c89a4931aa28cd0cb19ef5f4b294278d55c
Samantha Guergenenov <samguergen@gmail.com>
2016-11-30 13:14:19 -0500

    retrieving session is in erb page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
       session[:current_user_id] = @user.id
       puts "successfully logged in"
       puts session[:current_user_id]
-      redirect_to root_path
-      # redirect_to root_path({sess: session[:user_id], user: @user})
+      redirect_to root_path({sess: session[:current_user_id], usr: @user})
     else
       flash[:alert] = "You did not log in"
       puts "Not logged in"
